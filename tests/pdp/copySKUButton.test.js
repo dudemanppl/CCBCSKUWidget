48354f6a819ee87962afd44c9ca2352f12066de3
Tommy Liao <tommyliao340@hotmail.com>
2020-11-03 14:01:28 -0800

    [prog] Finishes pdp button tests

--- tests/pdp/copySKUButton.test.js
+++ tests/pdp/copySKUButton.test.js
@@ -7,13 +7,11 @@ const {
 } = require('../../src/pdp/copySKUButton');
 
 describe('copySKUButtonOnClick', () => {
-  const dummySKUElem = HTMLElem('div', ['js-selected-product-variant']);
-  document.body.append(dummySKUElem);
   global.navigator.clipboard = { writeText: jest.fn() };
 
-  // dummySKUElem.value = testSKU;
-
   describe('with no variant selected', () => {
+    const dummySKUElem = HTMLElem('div', ['js-selected-product-variant']);
+    document.body.append(dummySKUElem);
     const currentTarget = HTMLElem('div');
     const event = { currentTarget };
 
@@ -27,10 +25,91 @@ describe('copySKUButtonOnClick', () => {
       expect(currentTarget.textContent).toBe('Choose Item');
     });
   });
+
+  describe('with variant selected', () => {
+    clearBody();
+    const dummySKUElem = HTMLElem('div', ['js-selected-product-variant']);
+    dummySKUElem.value = testSKU;
+    document.body.append(dummySKUElem);
+    const currentTarget = HTMLElem('div');
+    const event = { currentTarget };
+    jest.useFakeTimers();
+
+    copySKUButtonOnClick(event);
+
+    test('should have correct class added', () => {
+      expect(currentTarget.classList[0]).toBe('flash');
+    });
+
+    test('should have correct text content', () => {
+      expect(currentTarget.textContent).toBe('Copied!');
+    });
+
+    test('should have copied SKU to clipboard', () => {
+      expect(global.navigator.clipboard.writeText).toHaveBeenCalledWith(
+        testSKU
+      );
+    });
+
+    describe('after cleanup', () => {
+      test('should have correct class removed', () => {
+        jest.runAllTimers();
+
+        expect([...currentTarget.classList]).toHaveLength(0);
+      });
+
+      test('should have correct text content', () => {
+        expect(currentTarget.textContent).toBe('Copied!');
+      });
+    });
+  });
 });
 
-describe('copySKUButtonOnMouseLeave', () => {});
+describe('copySKUButtonOnMouseLeave', () => {
+  const copySKUButton = HTMLElem('div');
+  copySKUButton.classList.add('no-variant-selected');
+  copySKUButton.textContent = '';
 
-describe('addMethodsToCopySKUButton', () => {});
+  copySKUButtonOnMouseLeave({ currentTarget: copySKUButton });
 
-describe('copySKUButton', () => {});
+  test('should have correct text content', () => {
+    expect(copySKUButton.textContent).toBe('Copy SKU');
+  });
+
+  test('should have correct class removed', () => {
+    expect([...copySKUButton.classList]).toHaveLength(0);
+  });
+});
+
+describe('addMethodsToCopySKUButton', () => {
+  const copySKUButton = HTMLElem('div');
+
+  addMethodsToCopySKUButton(copySKUButton);
+
+  test('mouseleave', () => {
+    expect(copySKUButton.onmouseleave).toBe(copySKUButtonOnMouseLeave);
+  });
+  test('onclick', () => {
+    expect(copySKUButton.onclick).toBe(copySKUButtonOnClick);
+  });
+});
+
+describe('copySKUButton', () => {
+  const newButton = copySKUButton();
+
+  test('should be a button', () => {
+    expect(newButton.tagName).toBe('BUTTON');
+  });
+
+  test('should have correct id', () => {
+    expect(newButton.id).toBe('copy-sku-button');
+  });
+
+  test('should have correct text content', () => {
+    expect(newButton.textContent).toBe('Copy SKU');
+  });
+
+  test('should have correct type attribute', () => {
+    expect(newButton.getAttribute('type')).toBe('button');
+  });
+});
