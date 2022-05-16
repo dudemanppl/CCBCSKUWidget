00e068a1f98f1f59fdbf71f179352a0a85c3b9cd
dudemanppl <tommyliao340@hotmail.com>
2022-05-16 11:10:21 -0700

    [prog] Updates ENV.ts to use updated format on live site

--- src/ENV.ts
+++ src/ENV.ts
@@ -6,12 +6,14 @@ const onCompetitiveCyclist: boolean =
   window.location.host === 'www.competitivecyclist.com';
 
 const siteString: string = onCompetitiveCyclist ? 'cc' : 'bc';
-const onPLP: boolean = Boolean(
-  document.getElementsByClassName('search-results').length
-);
-const onPDP: boolean = Boolean(
-  document.getElementsByClassName('js-kraken-pdp-body').length
-);
+
+const pageType: string = document
+  .getElementById('pageType')
+  .getAttribute('content')
+  .slice(0, 3);
+
+const onPLP: boolean = pageType === 'plp';
+const onPDP: boolean = pageType === 'pdp';
 
 export {
   queryString,
