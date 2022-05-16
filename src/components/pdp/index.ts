c4caab9e895e8294f3a4d3825f9c3f0701175f51
dudemanppl <tommyliao340@hotmail.com>
2022-05-16 10:59:04 -0700

    [prog] Changes PDP index.ts to be in line with main branch

--- src/components/pdp/index.ts
+++ src/components/pdp/index.ts
@@ -1,94 +1,26 @@
-import { HTMLElem, HTMLElemOptions } from '../../index';
+const WMSLinkPDP = (productID) => {
+  const newWMSLinkButton = HTMLElem('button', [
+    'chakra-button',
+    siteString,
+    'wms-button-pdp',
+  ]);
+  const newWMSLink = HTMLElem('a', ['wms-link-pdp'], null, 'Go to WMS');
 
-/**
- * Changes styling of dropdown options to reflect OOS status on CC PDP
- *
- * ONLY WORKS IN WINDOW CONTEXT
- */
+  newWMSLinkButton.setAttribute('type', 'button');
+  newWMSLink.href = `https://manager.backcountry.com/manager/admin/item_inventory.html?item_id=${productID}`;
 
-// const addOOSAlertToCCPDP = () => {
-//   for (const dropdownOption: HTMLElement of document.getElementsByClassName(
-//     'js-unifiedropdown-option'
-//   )) {
-//     const SKU: string = dropdownOption.getAttribute('sku-value');
+  newWMSLinkButton.append(newWMSLink);
 
-//     if (SKU && !BC.product.skusCollection[SKU].inventory) {
-//       dropdownOption.classList.add('oos-alert');
-//     }
-//   }
-// };
-
-class ScriptElem extends HTMLElem {
-  ScriptElem: HTMLScriptElement;
-
-  constructor(func: Function) {
-    const scriptElemOptions: HTMLElemOptions = {
-      tagName: 'script',
-    };
-
-    super(scriptElemOptions);
-
-    const functionAsString: string = this.anonFuncToStr(func);
-
-    this.ScriptElem = <HTMLScriptElement>this.HTMLElem;
-    this.ScriptElem.textContent = functionAsString;
-  }
-
-  /**
-   * @param {function} func Minified anonymous ES6 function with no args
-   * @return {string}
-   */
-
-  anonFuncToStr(func: Function): string {
-    return func.toString().slice(5, -1);
-  }
-}
-
-const invokeFuncInWindow = (func: Function) => {
-  const scriptElem: HTMLScriptElement = new ScriptElem(func);
-
-  document.head.append(scriptElem);
-
-  scriptElem.remove();
+  return newWMSLinkButton;
 };
 
-/**
- * @return {Element} Target location to add PDP buttons to
- */
-
-const PDPTargetLocation = () => {
-  const [targetLocation] = document.getElementsByClassName(
-    onCompetitiveCyclist ? 'add-to-cart' : 'js-buybox-actions'
-  );
-
-  return targetLocation;
-};
-
-/**
- * @return {string} Parent SKU of the PDP shown
- */
-
-const PDPProductID = () => {
-  const { value: productID } = document.querySelector(
-    '[name ="/atg/commerce/order/purchase/CartModifierFormHandler.productId"]'
-  );
-
-  return productID;
-};
-
-/* istanbul ignore next */
 if (onPDP) {
-  if (onCompetitiveCyclist) invokeFuncInWindow(addOOSAlertToCCPDP);
+  const PDPTargetLocation = document.getElementById('buybox-variant-selector')
+    .nextSibling.nextSibling;
 
-  PDPTargetLocation().append(WMSLink(PDPProductID()), copySKUButton());
+  const parentSKU = document
+    .querySelector('[data-id="productId"]')
+    .innerText.slice(-7);
+
+  PDPTargetLocation.append(WMSLinkPDP(parentSKU));
 }
-
-// removeIf(production)
-module.exports = {
-  addOOSAlertToCCPDP,
-  anonFuncToStr,
-  invokeFuncInWindow,
-  PDPTargetLocation,
-  PDPProductID,
-};
-// endRemoveIf(production)
